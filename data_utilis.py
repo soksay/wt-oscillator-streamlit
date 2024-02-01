2545cc421e70e132561c16eb2d7fad7e26c60075
Soksay Singvongsa <soksaysingvongsa@MacBook-Air-de-Soksay.local>
2024-02-01 13:03:46 +0100

    Update

---
+++ data_utilis.py
@@ -0,0 +1,126 @@
+import pandas as pd
+from google.cloud import storage
+from io import StringIO
+from datetime import datetime, timedelta
+import streamlit as st
+import numpy as np
+
+
+def read_csv_from_gcs_to_df(bucket_name, file_path):
+    """
+    Read a CSV file from a Google Cloud Storage bucket and return a DataFrame.
+
+    Args:
+        bucket_name (str): The name of the Google Cloud Storage bucket.
+        file_path (str): The path to the CSV file in the Google Cloud Storage bucket.
+
+    Returns:
+        pd.DataFrame: The DataFrame containing the data from the CSV file.
+    """
+    # Initialize a Cloud Storage client
+    client = storage.Client.from_service_account_info(st.secrets['SERVICE_ACC'])
+    # Get the bucket object
+    bucket = client.bucket(bucket_name)
+
+    # Get last update of the file
+    blobs_list = list(bucket.list_blobs())
+
+    for bl in blobs_list:
+        if bl.name == file_path:
+            last_update = bl.updated.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Read the CSV file from Cloud Storage
+    blob = bucket.blob(file_path)
+
+    csv_data = blob.download_as_string().decode('utf-8')
+
+    # Parse the CSV data into a DataFrame
+    dataframe = pd.read_csv(StringIO(csv_data))
+
+    return dataframe, last_update
+
+
+def calculate_nearest_fifteen_minute_timestamp(last_update):
+    last_update_datetime = datetime.strptime(last_update, '%Y-%m-%d %H:%M:%S')
+    if last_update_datetime.minute < 15:
+        nearest_fifteen_minute_timestamp = last_update_datetime.replace(minute=45, second=0, microsecond=0) - timedelta(hours=1)
+    elif 15 <= last_update_datetime.minute < 30:
+        nearest_fifteen_minute_timestamp = last_update_datetime.replace(minute=0, second=0, microsecond=0)
+    elif 30 <= last_update_datetime.minute < 45:
+        nearest_fifteen_minute_timestamp = last_update_datetime.replace(minute=15, second=0, microsecond=0)
+    else:
+        nearest_fifteen_minute_timestamp = last_update_datetime.replace(minute=30, second=0, microsecond=0)
+
+    nearest_fifteen_minute_timestamp = nearest_fifteen_minute_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    return nearest_fifteen_minute_timestamp
+
+def calculate_nearest_one_hour_timestamp(last_update):
+    last_update_datetime = datetime.strptime(last_update, '%Y-%m-%d %H:%M:%S')
+    nearest_one_hour_timestamp = (last_update_datetime - timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+    nearest_one_hour_timestamp = nearest_one_hour_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    return nearest_one_hour_timestamp
+
+def calculate_nearest_four_hour_timestamp(last_update):
+    last_update_datetime = datetime.strptime(last_update, '%Y-%m-%d %H:%M:%S')
+    if last_update_datetime.hour < 4:
+        nearest_four_hour_timestamp = last_update_datetime.replace(hour=20, minute=0, second=0, microsecond=0) - timedelta(day=1)
+    elif 4 <= last_update_datetime.hour < 8:
+        nearest_four_hour_timestamp = last_update_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+    elif 8 <= last_update_datetime.hour < 12:
+        nearest_four_hour_timestamp = last_update_datetime.replace(hour=4, minute=0, second=0, microsecond=0)
+    elif 12 <= last_update_datetime.hour < 16:
+        nearest_four_hour_timestamp = last_update_datetime.replace(hour=8, minute=0, second=0, microsecond=0)
+    elif 16 <= last_update_datetime.hour < 20:
+        nearest_four_hour_timestamp = last_update_datetime.replace(hour=12, minute=0, second=0, microsecond=0)
+    else:
+        nearest_four_hour_timestamp = last_update_datetime.replace(hour=16, minute=0, second=0, microsecond=0)
+    nearest_four_hour_timestamp = nearest_four_hour_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    return nearest_four_hour_timestamp
+
+def calculate_nearest_one_day_timestamp(last_update):
+    last_update_datetime = datetime.strptime(last_update, '%Y-%m-%d %H:%M:%S')
+    nearest_one_day_timestamp = (last_update_datetime - timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+    nearest_one_day_timestamp = nearest_one_day_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    return nearest_one_day_timestamp
+
+def calculate_nearest_one_week_timestamp(last_update):
+    last_update_datetime = datetime.strptime(last_update, '%Y-%m-%d %H:%M:%S')
+    nearest_one_week_timestamp = (last_update_datetime - timedelta(days=7) - timedelta(days=last_update_datetime.weekday()))
+    nearest_one_week_timestamp = nearest_one_week_timestamp.replace(hour=0, minute=0, second=0, microsecond=0)
+    return nearest_one_week_timestamp
+
+def create_timestamp_dataframe(fifteen_minutes,one_hour,four_hour,one_day,one_week):
+    timeframes_timestamp = {
+        '15m': [fifteen_minutes],
+        '1h': [one_hour],
+        '4h': [four_hour],
+        '1d': [one_day],
+        '1w': [one_week]
+    }
+    # Créer le DataFrame
+    timestamp_display_df = pd.DataFrame(timeframes_timestamp)
+    timestamp_df = timestamp_display_df.transpose().reset_index().rename(columns={'index': 'timeframe', 0: 'timestamp_datetime'})
+
+    return timestamp_display_df, timestamp_df
+
+
+def add_calculate_fields(dataframe):
+    dataframe['signal'] = np.where(dataframe['buy_signal'] == True, 'BUY', 'SELL')
+    dataframe['signal_timeframe'] = dataframe['signal'] + '_' + dataframe['timeframe']
+    dataframe['value_exists'] = True
+
+    return dataframe
+
+def create_signals_dataframe(dataframe):
+    # Pivot the table
+    pivot_table = dataframe.pivot_table(index='symbol', columns='signal_timeframe', values='value_exists', aggfunc='any',
+                                 fill_value=False)
+    pivot_table['COUNT SIGNALS'] = pivot_table.sum(axis=1)
+    pivot_table = pivot_table.reset_index()
+    pivot_table = pivot_table.reindex(columns=['symbol', 'BUY_15m', 'BUY_1h', 'BUY_4h', 'BUY_1d', 'BUY_1w', 'SELL_15m',
+                                               'SELL_1h', 'SELL_4h', 'SELL_1d', 'SELL_1w', 'COUNT SIGNALS'])
+    pivot_table = pivot_table.dropna(axis=1)
+
+    return pivot_table
+
+
--- main.py
+++ main.py
@@ -1,107 +1,41 @@
-import pandas as pd
-from google.cloud import storage
-from io import StringIO
-import numpy as np
-import streamlit as st
-from st_aggrid import AgGrid,GridOptionsBuilder
 
-def read_csv_from_gcs_to_df(bucket_name, file_path):
-    """
-    Read a CSV file from a Google Cloud Storage bucket and return a DataFrame.
+from data_utilis import *
 
-    Args:
-        bucket_name (str): The name of the Google Cloud Storage bucket.
-        file_path (str): The path to the CSV file in the Google Cloud Storage bucket.
+# get the data from gcs
+df, last_update = read_csv_from_gcs_to_df(st.secrets['BUCKET_NAME'], 'signals_df.csv')
 
-    Returns:
-        pd.DataFrame: The DataFrame containing the data from the CSV file.
-    """
-    # Initialize a Cloud Storage client
-    client = storage.Client.from_service_account_info(st.secrets['SERVICE_ACC'])
-    # Get the bucket object
-    bucket = client.bucket(bucket_name)
+# calculating nearest timestamps for each timeframe based on bucket last update
+fifteen_minute_timestamp = calculate_nearest_fifteen_minute_timestamp(last_update)
+one_hour_timestamp = calculate_nearest_one_hour_timestamp(last_update)
+four_hour_timestamp = calculate_nearest_four_hour_timestamp(last_update)
+one_day_timestamp = calculate_nearest_one_day_timestamp(last_update)
+one_week_timestamp = calculate_nearest_one_week_timestamp(last_update)
 
-    # Get last update of the file
-    blobs_list = list(bucket.list_blobs())
+timestamp_display_df, timestamp_df = create_timestamp_dataframe(fifteen_minute_timestamp, one_hour_timestamp,
+                                                                four_hour_timestamp, one_day_timestamp,
+                                                                one_week_timestamp)
+df = df.merge(timestamp_df,how='inner',on=['timeframe','timestamp_datetime'])
+df = add_calculate_fields(df)
 
-    for bl in blobs_list:
-        if bl.name == file_path:
-            last_update = bl.updated.strftime('%Y-%m-%d %H:%M:%S')
+# Pivot the table
+active_signals_df = create_signals_dataframe(df)
 
-    # Read the CSV file from Cloud Storage
-    blob = bucket.blob(file_path)
-
-    csv_data = blob.download_as_string().decode('utf-8')
-
-    # Parse the CSV data into a DataFrame
-    df = pd.read_csv(StringIO(csv_data))
-
-    return df, last_update
-
-df,last_update = read_csv_from_gcs_to_df(st.secrets['BUCKET_NAME'],'signals_temp_df.csv')
-df['signal'] = np.where(df['buy_signal'] == True,'BUY','SELL')
-mapping_timeframes = {
-    '15m':'1 - 15m',
-    '1h':'2 - 1h',
-    '4h':'3 - 4h',
-    '1d':'4 - 1d',
-    '1w':'5 - 1w'
-}
-
-df['timeframe_ordered'] = df['timeframe'].map(mapping_timeframes)
-
-st.markdown(f'<div style="text-align: center;">Wave trend oscillator signals calculated on Kucoin USDT (top 100 crypto from Coinmarketcap)</div>', unsafe_allow_html=True)
+st.markdown(
+    f'<div style="text-align: center;">Wave trend oscillator signals calculated on Kucoin USDT pairs (top 100 crypto from Coinmarketcap)</div>',
+    unsafe_allow_html=True)
 st.markdown(f'<div style="text-align: center;">last update : {last_update}</div>', unsafe_allow_html=True)
 st.write('----------------------------------------')
 
-gb = GridOptionsBuilder()
+# Adding a title to the DataFrame
+st.write('Timestamps to consider for each timeframe (refer to the opening timestamp of the candle)')
+st.dataframe(timestamp_display_df, hide_index=True,width=1000 )
 
-gb.configure_auto_height()
+on = st.toggle('Display raw data')
 
-# makes columns resizable, sortable and filterable by default
-gb.configure_default_column(
-    resizable=True,
-    filterable=True,
-    sortable=True,
-    editable=False,
-)
-
-#configures symbols column to have a 100px initial width
-gb.configure_column(field="symbol",
-                    header_name="Symbols",
-                    width=1,
-                    rowGroup=True
-                    )
-
-
-
-gb.configure_column(
-    field="signal",
-    header_name="signal",
-    pivot=True,
-)
-
-#configures timeframe column to have a tooltip and adjust to fill the grid container
-gb.configure_column(
-    field="timeframe_ordered",
-    header_name="timeframe",
-    pivot=True
-)
-
-
-gb.configure_column(
-    field="buy_signal",
-    header_name="nb_symbol",
-    type=["numericColumn"],
-    aggFunc="count",
-)
-#makes tooltip appear instantly
-gb.configure_grid_options(
-    tooltipShowDelay=0,
-    pivotMode=True,
-    suppressExpandablePivotGroups = True
-)
-go = gb.build()
-
-AgGrid(df, gridOptions=go)
+if on:
+    st.dataframe(
+        df[['symbol', 'timeframe', 'timestamp_datetime', 'signal', 'PriceUSD', 'WT1', 'WT2', 'MarketCapPosition']],
+        use_container_width=True, hide_index=True,width=1000, height=(len(df) + 1) * 35 + 3)
+else:
+    st.dataframe(active_signals_df, hide_index=True, width=1000, height=(len(active_signals_df) + 1) * 35 + 3)
 
